2e488b8c8ad89724c2fd2de8ab44355a45343b85
WagnerReis <wagnerreis1997@gmail.com>
2021-07-14 10:38:52 -0300

    Teste - Cadastro de usuário

---
+++ test/user.test.js
@@ -0,0 +1,21 @@
+let app = require("../src/app");
+let supertest = require("supertest");
+let request = supertest(app);
+
+describe("Cadastro de usuário", () => {
+
+  test("Deve cadastrar um usuário com sucesso",() => {
+    let time = Date.now();
+    let email = `${time}@gmail.com`;
+    let user = {name: "Wagner", email, password: "123456"};
+  });
+
+  return request.post("/user")
+    .send(user)
+    .then(res => {
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.email).toEqual(email);
+    }).catch(err => {
+      fail(err);
+    });
+});
